ce1c609781c103ebcab68900cf0a8b697d5942a2
Ian Hales <contact@ian-hales.com>
2021-01-30 13:56:06 +0000

    Split pages registration into a list of pages and a loop to make it cleaner when adding more pages.

--- src/pages.ts
+++ src/pages.ts
@@ -1,4 +1,4 @@
-import MenuRegistry from "./menu-registry";
+import MenuRegistry, {PageDef} from "./menu-registry";
 import BarChartIcon from "@material-ui/icons/BarChart";
 import MonitorPage from "./components/adapters/sysmon";
 import SettingsRemoteIcon from "@material-ui/icons/SettingsRemote";
@@ -6,39 +6,40 @@ import LGTVRemotePage from "./components/pages/lgtv-remote";
 import KasaControllerPage from "./components/adapters/kasa";
 import TvIcon from "@material-ui/icons/Tv";
 
-// TODO: Load this from a config file
 
-MenuRegistry.getInstance().register({
-    url: "/",
-    name: "System Stats",
-    icon: BarChartIcon,
-    component: MonitorPage,
-    props: {
-        urls: [
-            "http://192.168.1.126:7653",
-            // "http://192.168.1.202:7653"
-        ]
+const pages: Array<PageDef<any>> = [
+    {
+        url: "/",
+        name: "System Stats",
+        icon: BarChartIcon,
+        component: MonitorPage,
+        props: {
+            urls: [
+                "http://192.168.1.126:7653",
+                "http://192.168.1.202:7653"
+            ]
+        }
+    },
+    {
+        url: "/lgtv-remote",
+        name: "TV Remote",
+        icon: SettingsRemoteIcon,
+        component: LGTVRemotePage,
+        props: {
+            text: "This is some demo text."
+        }
+    },
+    {
+        url: "/kasa-controller",
+        name: "Kasa Devices",
+        icon: TvIcon,
+        component: KasaControllerPage,
+        props: {
+            kasaText: "This is some demo text."
+        }
     }
-});
+];
 
-MenuRegistry.getInstance().register({
-    url: "/lgtv-remote",
-    name: "TV Remote",
-    icon: SettingsRemoteIcon,
-    component: LGTVRemotePage,
-    props: {
-        text: "This is some demo text."
-    }
-});
-
-MenuRegistry.getInstance().register({
-    url: "/kasa-controller",
-    name: "Kasa Devices",
-    icon: TvIcon,
-    component: KasaControllerPage,
-    props: {
-        kasaText: "This is some demo text."
-    }
-})
+pages.forEach((page) => MenuRegistry.getInstance().register(page));
 
 export default MenuRegistry.getInstance().getPages();
